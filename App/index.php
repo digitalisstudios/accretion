a991ce313bc075b4eb130b76bc51e086c5c6d2f9
dvicemuse <dvicemuse@gmail.com>
2017-07-25 02:43:29 -0700

    updates to namespaces

--- App/index.php
+++ App/index.php
@@ -1,8 +1,5 @@
 <?php
 
-	ini_set('display_errors', '1');
-	error_reporting(E_ALL);
-
 	//GET THE LOADER FILE	
 	require dirname(__FILE__).'/system/Accretion.php';
 
--- App/system/Accretion.php
+++ App/system/Accretion.php
@@ -1,7 +1,5 @@
 <?php
 
-    namespace App;
-
     class Accretion {
 
         //GLOBAL VARIABLES
