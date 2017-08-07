168ffc124ae8e2ab98b78ae327469fe8726415ab
dvicemuse <dvicemuse@gmail.com>
2017-08-07 02:07:22 -0700

    updates to encryption

--- system/helper/Encryption.php
+++ system/helper/Encryption.php
@@ -1,16 +1,5 @@
 <?php 
 	class Encryption_Helper extends Helper {
-
-		public static $use_mcrypt = true;
-
-		public static function use_mcrypt(){
-			if(!function_exists('mcrypt_get_iv_size')){
-				\Encryption_Helper::$use_mcrypt = true;
-			}
-			else{
-				\Encryption_Helper::$use_mcrypt = false;
-			}
-		}
 		
 		public static function encrypt($string){
 		    return openssl_encrypt($string, "AES-128-ECB", pack('H*', Config::get('encryption_key')));
