b26430762f9da508dec65a3121e5369d706d0857
dvicemuse <dvicemuse@gmail.com>
2017-08-07 02:06:34 -0700

    updates to the framework

--- system/helper/Encryption.php
+++ system/helper/Encryption.php
@@ -1,28 +1,23 @@
 <?php 
 	class Encryption_Helper extends Helper {
+
+		public static $use_mcrypt = true;
+
+		public static function use_mcrypt(){
+			if(!function_exists('mcrypt_get_iv_size')){
+				\Encryption_Helper::$use_mcrypt = true;
+			}
+			else{
+				\Encryption_Helper::$use_mcrypt = false;
+			}
+		}
 		
 		public static function encrypt($string){
-			
-		    $key = pack('H*', Config::get('encryption_key'));
-		    $key_size 			= strlen($key);
-		    $iv_size 			= mcrypt_get_iv_size(MCRYPT_RIJNDAEL_128, MCRYPT_MODE_CBC);
-		    $iv 				= mcrypt_create_iv($iv_size, MCRYPT_RAND);
-		    $ciphertext 		= mcrypt_encrypt(MCRYPT_RIJNDAEL_128, $key, $string, MCRYPT_MODE_CBC, $iv);
-		    $ciphertext 		= $iv . $ciphertext;
-		    $ciphertext_base64 	= base64_encode($ciphertext);
-		    return $ciphertext_base64;
+		    return openssl_encrypt($string, "AES-128-ECB", pack('H*', Config::get('encryption_key')));
 		}
 
 		public static function decrypt($string){
-			
-			$key = pack('H*', Config::get('encryption_key'));
-			$ciphertext_dec 	= base64_decode($string);
-			$iv_size 			= mcrypt_get_iv_size(MCRYPT_RIJNDAEL_128, MCRYPT_MODE_CBC);
-		    $iv_dec 			= substr($ciphertext_dec, 0, $iv_size);
-		    $ciphertext_dec 	= substr($ciphertext_dec, $iv_size);
-		    $plaintext_dec 		= mcrypt_decrypt(MCRYPT_RIJNDAEL_128, $key, $ciphertext_dec, MCRYPT_MODE_CBC, $iv_dec);
-		    return $plaintext_dec;
-
+			return openssl_decrypt($string,"AES-128-ECB", pack('H*', Config::get('encryption_key')));
 		}
 
 		public static function generate_key(){
